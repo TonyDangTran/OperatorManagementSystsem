9b183d9472d14309d0d1d458c105d7efb4bfed86
TonyDangTran <mdan759@aucklanduni.ac.nz>
2025-04-15 17:38:50 +1200

    Started implementation of createOperator method using Operator class

--- src/main/java/nz/ac/auckland/se281/Operator.java
+++ src/main/java/nz/ac/auckland/se281/Operator.java
@@ -1,4 +1,48 @@
 package nz.ac.auckland.se281;
 
+public class Operator {
+  private String operatorName;
+  private String location;
+  private String operatorInitials = "";
+  private String operatorID = "";
+  private Types.Location locationEnum;
 
-public class Operator {}
+  public Operator(String operatorID, String operatorName, String location) {
+    this.operatorID = operatorID;
+    this.operatorName = operatorName;
+    this.location = location;
+    this.locationEnum = Types.Location.fromString(this.location);
+  }
+
+  public Operator(String operatorName, String location) {
+    this.operatorName = operatorName;
+    this.location = location;
+    this.locationEnum = Types.Location.fromString(this.location);
+  }
+
+  public String getOperatorID() {
+    operatorID = getOperatorInitials() + "-" + locationEnum.getLocationAbbreviation() + "-" + "001";
+    return operatorID;
+  }
+
+  public String getOperatorName() {
+    if ((this.operatorName == null) || (this.operatorName.equals("null"))) {
+      return MessageCli.OPERATOR_NOT_CREATED_INVALID_OPERATOR_NAME.getMessage(operatorName);
+    }
+    return operatorName;
+  }
+
+  public String getLocation() {
+
+    return locationEnum.getFullName();
+  }
+
+  public String getOperatorInitials() {
+    String[] Initials = this.operatorName.split(" ");
+
+    for (String word : Initials) {
+      operatorInitials += (word.charAt(0));
+    }
+    return operatorInitials;
+  }
+}
--- src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
+++ src/main/java/nz/ac/auckland/se281/OperatorManagementSystem.java
@@ -1,16 +1,32 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 public class OperatorManagementSystem {
+
+  private String keyword;
+  private String operatorName;
+  private String location;
+  private ArrayList<Operator> operatorList = new ArrayList<>();
 
   // Do not change the parameters of the constructor
   public OperatorManagementSystem() {}
 
   public void searchOperators(String keyword) {
+    System.out.println("There are no matching operators found.");
     // TODO implement
   }
 
   public void createOperator(String operatorName, String location) {
-    // TODO implement
+    this.operatorName = operatorName;
+    this.location = location;
+    Operator newOperator = new Operator(this.operatorName, this.location);
+    operatorList.add(newOperator);
+    System.out.println(
+        MessageCli.OPERATOR_CREATED.getMessage(
+            newOperator.getOperatorName(), newOperator.getOperatorID(), newOperator.getLocation()));
+    // operatorList.add(MessageCli.OPERATOR_ENTRY.getMessage(storedOperator, operatorID,
+    // storedLocation.toString()));
   }
 
   public void viewActivities(String operatorId) {
